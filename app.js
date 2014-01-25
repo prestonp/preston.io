5f40c5ef2ea632fff2a4c7e4bafc09b2bc05f407
Preston Pham <prestonp08@gmail.com>
2014-01-25 17:48:39 -0600

    fix gravatar favicon

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.favicon(gravatar.url('prestonp08@gmail.com', {s: 16}, https=false)));
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.locals.gravatar = gravatar.url('prestonp08@gmail.com', {s: 16}, https=false);
   app.use(app.router);
   app.use(require('less-middleware')({ src: __dirname + '/public' }));
   app.use(express.static(path.join(__dirname, 'public')));
