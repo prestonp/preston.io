51a0a2c305cd2009dafd424f28720bb9431efdfb
Preston Pham <prestonp08@gmail.com>
2014-01-25 17:39:32 -0600

    set gravatar as favicon

--- app.js
+++ app.js
@@ -5,9 +5,9 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , gravatar = require('gravatar');
 
 var app = express();
 
@@ -15,7 +15,7 @@ app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.favicon());
+  app.use(express.favicon(gravatar.url('prestonp08@gmail.com', {s: 16}, https=false)));
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
@@ -29,7 +29,6 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
