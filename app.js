76e2da9794de97ccc0a4864e332d0906d98cb90e
Preston Pham <prestonp08@gmail.com>
2015-06-28 20:01:38 -0500

    omg crazy update

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ var express = require('express')
   , path = require('path')
   , gravatar = require('gravatar');
 
+/**
+ * Middleware
+ */
+var theme = require('./lib/theme');
+
 var app = express();
 
 app.configure(function(){
@@ -29,7 +34,8 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
-app.get('/projects', routes.projects);
+app.get('/work', theme('light'), routes.work);
+app.get('/about', theme('light'), routes.about);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
